e92fefbf26661de2d6ab700de9b51ecc0f325337
Efcolpit <45362427+Efcolpit@users.noreply.github.com>
2020-11-09 21:11:07 +0300

    add composer

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-use application\core\Router;
+use Application\Core\Router;
 
 spl_autoload_register(function($class){
 	$path = str_replace('\\','/',$class.'.php');
