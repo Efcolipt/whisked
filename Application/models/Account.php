f11a5b949867298aee4f2e2c7ca02f207e9f83aa
Efcolipt <efcolipt@gmail.com>
2021-02-16 20:06:11 +0300

    fix

--- Application/models/Account.php
+++ Application/models/Account.php
@@ -89,6 +89,12 @@ class Account extends Model
 	}
 
 
+	public function getUser($login){
+		$params = ['login' => Helper::filterString($login)];
+		$user = $this->db->row('SELECT * FROM users WHERE login = :login',$params);
+		return $user;
+	}
+
 	public function userEditInfo(){
 		$data = $_POST;
 		$MessageError = [];
