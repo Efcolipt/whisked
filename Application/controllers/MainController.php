d42096aa30c4f26d9e22002e6dd9fce04c66b249
Efcolipt <efcolipt@gmail.com>
2021-03-20 01:37:08 +0300

    Added vue, changed search(on vue)

--- Application/controllers/MainController.php
+++ Application/controllers/MainController.php
@@ -8,18 +8,18 @@ use Application\core\View;
 
 class MainController extends  Controller{
 
-	public function homeAction()
-	{
-		$upcoming = Helper::getContentWithBuildQuery($this->urlContentMain,['token'=> $this->urlTokenContent, 'type'=>'film','year'=>'2020','resolution'=>'2160']);
-		$serials = Helper::getContentWithBuildQuery($this->urlContentMain,['token'=> $this->urlTokenContent, 'type'=>'serial','year'=>'2020','resolution'=>'1080']);
-		$anime = Helper::getContentWithBuildQuery($this->urlContentMain,['token'=> $this->urlTokenContent, 'type'=>'film','year'=>'2019','cat'=>'аниме']);
-		if (!$upcoming || !$serials || !$anime)  View::errorCode(404);
-		$this->view->render('Главная',$vars = [
-			'upcoming' => $upcoming['results'],
-			'serials' => $serials['results'],
-			'anime' => $anime['results'],
-		]);
-	}
+	// public function homeAction()
+	// {
+	// 	$upcoming = Helper::getContent($this->urlContentMain,['token'=> $this->urlTokenContent, 'type'=>'film','year'=>'2020','resolution'=>'2160']);
+	// 	$serials = Helper::getContent($this->urlContentMain,['token'=> $this->urlTokenContent, 'type'=>'serial','year'=>'2020','resolution'=>'1080']);
+	// 	$anime = Helper::getContent($this->urlContentMain,['token'=> $this->urlTokenContent, 'type'=>'film','year'=>'2019','cat'=>'аниме']);
+	// 	if (!$upcoming || !$serials || !$anime)  View::errorCode(404);
+	// 	$this->view->render('Главная',$vars = [
+	// 		'upcoming' => $upcoming['results'],
+	// 		'serials' => $serials['results'],
+	// 		'anime' => $anime['results'],
+	// 	]);
+	// }
 
 
 	public function rightAction()
--- Application/views/site_closed/closed.php
+++ Application/views/site_closed/closed.php
@@ -4,10 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" href="/public/images/favicon/favicon.png" >
-  <link rel="stylesheet" href="/public/css/error.css">
-		<!-- Np-progress -->
-	<script src="/public/packages/np-progress/nprogress.js"></script>
-	<link rel="stylesheet" type="text/css" href="/public/packages/np-progress/nprogress.css" />
+  	<link rel="stylesheet" href="/public/css/error.min.css">
 	<title>На странице ведустя технические работы</title>
 </head>
 <body>
@@ -16,14 +13,11 @@
 			<div class="error_content">
   			<h1>Страница закрыта</h1>
 				<p>Страница в разработке, она скоро будет доступна - возвращайтесь!</p>
-				<a href="/" class="back_home">
+				<a href="/movies" class="back_home">
 					<img src="/public/images/errors/arrow-back.png" alt="Назад">
-					<span>Вернуться на главную</span>
+					<span>Назад к фильмам</span>
 				</a>
 				<ul class="list_menu">
-					<li class="list_menu_item">
-						<a href="/" class="list_menu_link">Главная</a>
-					</li>
 					<li class="list_menu_item">
 						<a href="/movies" class="list_menu_link">Фильмы</a>
 					</li>
@@ -40,7 +34,5 @@
 			</div>
 		</div>
 	</div>
-	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="crossorigin="anonymous"></script>
-	<script src="/public/js/error_page.js"></script>
 </body>
 </html>
