7c9a1734b558d62c7db27ec10723868f93460cb7
Efcolipt <efcolipt@gmail.com>
2021-05-31 00:23:53 +0300

    switch by dev on prod version vue

--- Application/views/layouts/default.php
+++ Application/views/layouts/default.php
@@ -192,8 +192,8 @@
 		</footer>
 	</div>
 	<script async src="https://kit.fontawesome.com/de8f891afd.js"></script>
-	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="crossorigin="anonymous"></script>
-	<script src="https://unpkg.com/vue@next"></script>
+	<script  src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/vue@3.0.10/dist/vue.global.prod.js"></script>
 	<script src="/public/js/app.js"></script>
 	<script src="/public/js/view.js"></script>
 </body>
