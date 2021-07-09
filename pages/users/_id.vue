7c3d73100275ade023b5d4bc323d490008232c0f
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-07-09 16:47:25 +0300

    init

---
+++ pages/users/_id.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+    <div class="">
+        {{users}}
+    </div>
+</template>
+
+<script>
+export default {
+    async asyncData({ $axios, params }) {
+        const users = await $axios.$get('/api/users/' + params.id)
+
+        return {
+          users,
+        }
+  },
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ pages/users/index.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+    <div class="">
+        {{ users }}
+    </div>
+</template>
+
+<script>
+export default {
+    async asyncData({ $axios, params }) {
+        const users = await $axios.$get('/api/users')
+
+        return {
+          users,
+        }
+  },
+}
+</script>
+
+<style lang="css" scoped>
+</style>
